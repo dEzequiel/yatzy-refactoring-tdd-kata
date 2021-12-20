dfdc5dc6c8c52bb1f8c1ce587946a2172684b82f
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2021-12-20 18:34:05 +0100

    Revert "refactor(Yatzy.large_straight)"
    This reverts commit 3987e795c2f663ed4b0495d6cc792bbd35905fa5.

--- yatzy.py
+++ yatzy.py
@@ -114,20 +114,26 @@ class Yatzy:
             lista.append(n)
         
         if dice == lista:
-            return Yatzy.chance(dice)
+            return 15
         else:
             return 0
-        
-        
+    
+
     @staticmethod
-    def large_straight(dice):  
-        
-            
-        for i in range(2, 7):
-            if dice.count(i) != 1:  # Si un numero aparece mas de una vez ya no es una secuencia seguida
-                return 0
-        return Yatzy.chance(dice)
-        
+    def largeStraight( d1,  d2,  d3,  d4,  d5):
+        tallies = [0]*6
+        tallies[d1-1] += 1
+        tallies[d2-1] += 1
+        tallies[d3-1] += 1
+        tallies[d4-1] += 1
+        tallies[d5-1] += 1
+        if (tallies[1] == 1 and
+            tallies[2] == 1 and
+            tallies[3] == 1 and
+            tallies[4] == 1
+            and tallies[5] == 1):
+            return 20
+        return 0
     
 
     @staticmethod
