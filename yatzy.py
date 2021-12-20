3ed8777b1cd8779f0c12c16e090c1412406854d5
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2021-12-20 17:10:29 +0100

    refactor(Yatzy.small_straight)

--- yatzy.py
+++ yatzy.py
@@ -106,20 +106,17 @@ class Yatzy:
         return 0    
     
     @staticmethod
-    def smallStraight( d1,  d2,  d3,  d4,  d5):
-        tallies = [0]*6
-        tallies[d1-1] += 1
-        tallies[d2-1] += 1
-        tallies[d3-1] += 1
-        tallies[d4-1] += 1
-        tallies[d5-1] += 1
-        if (tallies[0] == 1 and
-            tallies[1] == 1 and
-            tallies[2] == 1 and
-            tallies[3] == 1 and
-            tallies[4] == 1):
+    def small_straight(dice):
+        
+        # Generates an already sorted list to compare with given list.
+        lista = []
+        for n in range(1, 6):
+            lista.append(n)
+        
+        if dice == lista:
             return 15
-        return 0
+        else:
+            return 0
     
 
     @staticmethod
