314d13e819f81169332e994aa42fd73481684c5f
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2021-12-24 19:32:38 +0100

    refactor: black

--- yatzt-test.py
+++ yatzt-test.py
@@ -18,81 +18,92 @@ def test_yatzy():
     assert 0 == Yatzy.yatzy([1, 3, 1, 1, 1])
     assert 50 == Yatzy.yatzy([2, 2, 2, 2, 2])
 
+
 def test_ones():
     assert 2 == Yatzy.ones([1, 3, 3, 1, 5])
-    assert 5 == Yatzy.ones([1, 1, 1, 1, 1]) 
+    assert 5 == Yatzy.ones([1, 1, 1, 1, 1])
     assert 0 == Yatzy.ones([2, 3, 4, 5, 6])
-    
+
+
 def test_twos():
     assert 0 == Yatzy.twos([3, 3, 3, 4, 5])
-    assert 10 == Yatzy.twos([2, 2, 2, 2, 2]) 
-    assert 4 == Yatzy.twos([1, 3, 2, 4, 2]) 
+    assert 10 == Yatzy.twos([2, 2, 2, 2, 2])
+    assert 4 == Yatzy.twos([1, 3, 2, 4, 2])
+
 
 def test_threes():
     assert 9 == Yatzy.threes([3, 3, 3, 4, 5])
-    assert 15 == Yatzy.threes([3, 3, 3, 3, 3]) 
+    assert 15 == Yatzy.threes([3, 3, 3, 3, 3])
     assert 0 == Yatzy.threes([1, 2, 4, 5, 6])
+
 
 def test_fours():
     object1 = Yatzy([4, 4, 4, 4, 4])
     result1 = object1.fours()
     assert result1 == 20
-    
+
     object2 = Yatzy([1, 2, 3, 4])
     result2 = object2.fours()
     assert result2 == 4
-    
+
     object3 = Yatzy([1, 2, 3, 5])
     result3 = object3.fours()
     assert result3 == 0
+
 
 def test_fives():
     object1 = Yatzy([5, 5, 5, 5, 5])
     result1 = object1.fives()
     assert result1 == 25
-    
+
     object2 = Yatzy([1, 2, 3, 4, 5])
     result2 = object2.fives()
     assert result2 == 5
-    
+
     object3 = Yatzy([1, 2, 3, 4, 6])
     result3 = object3.fives()
     assert result3 == 0
+
 
 def test_sixes():
     object1 = Yatzy([6, 6, 6, 6, 6])
     result1 = object1.sixes()
     assert result1 == 30
-    
+
     object2 = Yatzy([1, 2, 3, 4, 6])
     result2 = object2.sixes()
     assert result2 == 6
-    
+
     object3 = Yatzy([1, 2, 3, 4, 5])
     result3 = object3.sixes()
     assert result3 == 0
 
+
 def test_score_pair():
     assert 10 == Yatzy.score_pair([2, 2, 2, 5, 5])
     assert 12 == Yatzy.score_pair([5, 5, 6, 6, 1])
-    assert 0  == Yatzy.score_pair([1, 2, 3, 4, 5])
+    assert 0 == Yatzy.score_pair([1, 2, 3, 4, 5])
+
 
 def test_two_pair():
     assert 8 == Yatzy.two_pair([1, 1, 2, 3, 3])
     assert 0 == Yatzy.two_pair([2, 2, 1, 3, 4])
     assert 6 == Yatzy.two_pair([1, 1, 2, 2, 3])
 
-def test_four__of_akind ():
+
+def test_four__of_akind():
     assert 8 == Yatzy.four_of_a_kind([2, 2, 2, 2, 5])
     assert 0 == Yatzy.four_of_a_kind([1, 2, 3, 4, 5])
 
+
 def test_three_of_a_kind():
-    assert 12 == Yatzy.three_of_a_kind([3,4,4,4,5])
+    assert 12 == Yatzy.three_of_a_kind([3, 4, 4, 4, 5])
     assert 0 == Yatzy.three_of_a_kind([5, 5, 5, 5, 4])
-    assert 9 == Yatzy.three_of_a_kind([6, 5, 3, 3, 3]) 
-    assert 15 == Yatzy.three_of_a_kind([5, 1, 2, 5, 5])  
+    assert 9 == Yatzy.three_of_a_kind([6, 5, 3, 3, 3])
+    assert 15 == Yatzy.three_of_a_kind([5, 1, 2, 5, 5])
     assert 3 == Yatzy.three_of_a_kind([1, 1, 1, 4, 3])
-    
+
+
 def test_small_straight():
     assert 0 == Yatzy.small_straight([1, 2, 3, 3, 5])
     assert 15 == Yatzy.small_straight([1, 2, 3, 4, 5])
@@ -101,12 +112,14 @@ def test_small_straight():
     assert 0 == Yatzy.small_straight([6, 6, 6, 6, 6])
     assert 0 == Yatzy.small_straight([1, 2, 3, 4, 6])
 
+
 def test_large_straight():
     assert 20 == Yatzy.large_straight([2, 3, 4, 5, 6])
     assert 0 == Yatzy.large_straight([1, 2, 3, 4, 5])
     assert 0 == Yatzy.large_straight([1, 3, 4, 5, 5])
     assert 0 == Yatzy.large_straight([6, 6, 6, 6, 6])
     assert 0 == Yatzy.large_straight([1, 2, 3, 4, 6])
+
 
 # @pytest.fixture
 # def inyector():
--- yatzy.py
+++ yatzy.py
@@ -1,29 +1,27 @@
-from typing import Dict, NoReturn
-
-
 class Yatzy:
-
     def __init__(self, dice):
         self.dice = list(dice)
-    
+
     @staticmethod
     def chance(dice):
-        
+
         total = 0
         for value in dice:
             total += value
         return total
 
     @staticmethod
-    def yatzy(dice):       
+    def yatzy(dice):
         check = True
         for value in dice:
             if dice[0] != dice[value]:
                 check = False
-                break;
-        if (check == True): return 50
-        else: return 0
-    
+                break
+        if check == True:
+            return 50
+        else:
+            return 0
+
     @staticmethod
     def ones(dice):
         counter = dice.count(1)
@@ -33,104 +31,99 @@ class Yatzy:
     def twos(dice):
         counter = dice.count(2)
         return counter * 2
-    
+
     @staticmethod
     def threes(dice):
         counter = dice.count(3)
         return counter * 3
-    
+
     # metodo de instancia
     def fours(self):
         counter = self.dice.count(4)
-        return counter * 4    
+        return counter * 4
 
     def fives(self):
         counter = self.dice.count(5)
         return counter * 5
-    
 
     def sixes(self):
         counter = self.dice.count(6)
         return counter * 6
-    
+
     @staticmethod
     def score_pair(dice):
         max_value = max(dice)
         if dice.count(max_value) == 2:
-                return max_value * 2
-        else:    
+            return max_value * 2
+        else:
             for value in dice:
                 if dice.count(value) == 2:
                     return value * 2
                 else:
                     return 0
-    
+
     @staticmethod
     def two_pair(dice):
-        
+
         pairs = []
         for value in dice:
             if dice.count(value) == 2:
                 pairs.append(value)
-        
+
         if len(pairs) == 1:
             return 0
-        
+
         pairs.pop()
         pairs.pop(0)
-        
+
         sum = 0
         print(pairs)
         for value in pairs:
             sum += value * 2
-        
+
         return sum
-                       
-            
-            
+
     @staticmethod
     def four_of_a_kind(dice):
-    
+
         for i in dice:
             if dice.count(i) == 4:
                 return i * 4
-            
+
             return 0
-        
+
     @staticmethod
     def three_of_a_kind(dice):
-        
+
         for i in dice:
             if dice.count(i) == 3:
                 return i * 3
-        return 0    
-    
+        return 0
+
     @staticmethod
     def small_straight(dice):
-        
+
         # Generates an already sorted list to compare with given list.
         lista = []
         for n in range(1, 6):
             lista.append(n)
-        
+
         if dice == lista:
             return 15
         else:
             return 0
-    
 
     @staticmethod
     def large_straight(dice):
-        
+
         for i in range(2, 7):
             if dice.count(i) != 1:  # If number appear more than one, is not a sequence.
                 return 0
-        
+
         return Yatzy.chance(dice)
-    
 
     @staticmethod
-    def fullHouse( d1,  d2,  d3,  d4,  d5):
+    def fullHouse(d1, d2, d3, d4, d5):
         tallies = []
         _2 = False
         i = 0
@@ -138,26 +131,24 @@ class Yatzy:
         _3 = False
         _3_at = 0
 
-        tallies = [0]*6
-        tallies[d1-1] += 1
-        tallies[d2-1] += 1
-        tallies[d3-1] += 1
-        tallies[d4-1] += 1
-        tallies[d5-1] += 1
+        tallies = [0] * 6
+        tallies[d1 - 1] += 1
+        tallies[d2 - 1] += 1
+        tallies[d3 - 1] += 1
+        tallies[d4 - 1] += 1
+        tallies[d5 - 1] += 1
 
         for i in range(6):
-            if (tallies[i] == 2): 
+            if tallies[i] == 2:
                 _2 = True
-                _2_at = i+1
-            
+                _2_at = i + 1
 
         for i in range(6):
-            if (tallies[i] == 3): 
+            if tallies[i] == 3:
                 _3 = True
-                _3_at = i+1
-            
+                _3_at = i + 1
 
-        if (_2 and _3):
+        if _2 and _3:
             return _2_at * 2 + _3_at * 3
         else:
             return 0
