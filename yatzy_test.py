43c4fcde4e44c67b3aa6747a60ea9c5b5e8190e9
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2022-01-06 18:40:09 +0100

    refactor: add some tests

--- yatzy_test.py
+++ yatzy_test.py
@@ -105,7 +105,9 @@ def test_large_straight():
     assert 0 == Yatzy.large_straight([1, 3, 4, 5, 5])
     assert 0 == Yatzy.large_straight([6, 6, 6, 6, 6])
     assert 0 == Yatzy.large_straight([1, 2, 3, 4, 6])
-
+    assert 20 == Yatzy.large_straight([6, 2, 3, 4, 5])
+    assert 20 == Yatzy.large_straight([2,3,4,5,6])
+    assert 0 == Yatzy.large_straight([1,2,2,4,5])
 
 def test_full_house():
     assert 8 == Yatzy.full_house([1, 1, 2, 2, 2])
