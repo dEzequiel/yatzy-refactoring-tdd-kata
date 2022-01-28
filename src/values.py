0b0d93f953709ab28fdc7bfde7c78f73cd3de3af
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-01-28 11:38:44 +0100

    feat(values): create enumerate class to represent constant integers values

---
+++ src/values.py
@@ -0,0 +1,13 @@
+from enum import IntEnum, unique
+
+@unique
+class Value(IntEnum):
+    ZERO = 0
+    ONE = 1
+    TWO = 2
+    THREE = 3
+    FOUR = 4
+    FIVE = 5
+    SIX = 6
+    
+    TOTAL_SCORE = 50
