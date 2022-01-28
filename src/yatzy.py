67e4a5e0a8f6bcd48515e7669b82504a6b338b04
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-01-28 11:39:09 +0100

    refactor: usage of Value enumerate class for int values

--- src/yatzy.py
+++ src/yatzy.py
@@ -1,12 +1,5 @@
 from src.pips import Pips
-
-ZERO = 0
-ONE = 1
-TWO = 2
-THREE = 3
-FOUR = 4
-FIVE = 5
-SIX = 6
+from src.values import Value
 
 class Yatzy:
     
@@ -25,47 +18,44 @@ class Yatzy:
         so if the returned list length is 1, indicates all 5 elements 
         where repeated and only contains one of this elements
         '''
-        TOTAL_SCORE = 50
-        JUST_ONE_ELEMENT = 1
-        
-        return TOTAL_SCORE if len(set(dice)) == JUST_ONE_ELEMENT else ZERO
+        return Value.TOTAL_SCORE if len(set(dice)) == Value.ONE else Value.ZERO
     
     @staticmethod
     def ones(dice):
         ONE_PIP = Pips.ONE.value
-        return dice.count(ONE_PIP) * ONE
+        return dice.count(ONE_PIP) * Value.ONE
 
     @staticmethod
     def twos(dice):
         TWO_PIP = Pips.TWO.value
-        return dice.count(TWO_PIP) * TWO
+        return dice.count(TWO_PIP) * Value.TWO
 
     @staticmethod
     def threes(dice):
         THREE_PIP = Pips.THREE.value
-        return dice.count(THREE_PIP) * THREE
+        return dice.count(THREE_PIP) * Value.THREE
 
     @staticmethod
     def fours(dice):
         FOUR_PIP = Pips.FOUR.value
-        return dice.count(FOUR_PIP) * FOUR
+        return dice.count(FOUR_PIP) * Value.FOUR
 
     @staticmethod
     def fives(dice):
         FIVE_PIP = Pips.FIVE.value
-        return dice.count(FIVE_PIP) * FIVE
+        return dice.count(FIVE_PIP) * Value.FIVE
 
     @staticmethod
     def sixes(dice):
         SIX_PIP = Pips.SIX.value
-        return dice.count(SIX_PIP) * SIX
+        return dice.count(SIX_PIP) * Value.SIX
 
     @staticmethod
     def score_pair(dice):
         for pip in Pips.reversed_values():
-            if dice.count(pip) >= TWO:
-                return pip * TWO
-        return ZERO
+            if dice.count(pip) >= Value.TWO:
+                return pip * Value.TWO
+        return Value.ZERO
 
         # REVERSED_DICE = Pips.reversed_values()
         # return Yatzy.chance(filter((lambda pip: pip * TWO if REVERSED_DICE.count(pip) >= TWO else ZERO), REVERSED_DICE))
@@ -73,41 +63,39 @@ class Yatzy:
     @staticmethod
     def two_pair(dice):
 
-        PAIRS_LIST = set(list(filter((lambda pip: dice.count(pip) >= TWO), dice)))
-        return sum(PAIRS_LIST) * TWO if len(PAIRS_LIST) == TWO else ZERO
+        PAIRS_LIST = set(list(filter((lambda pip: dice.count(pip) >= Value.TWO), dice)))
+        return sum(PAIRS_LIST) * Value.TWO if len(PAIRS_LIST) == Value.TWO else Value.ZERO
 
     @staticmethod
     def three_of_a_kind(dice):
         
-        threesomes = list(filter((lambda value: dice.count(value) >= THREE), dice))        
-        return threesomes[Pips.ONE.value] * THREE if len(threesomes) != ZERO else ZERO
+        threesomes = list(filter((lambda value: dice.count(value) >= Value.THREE), dice))        
+        return threesomes[Pips.ONE.value] * Value.THREE if len(threesomes) != Value.ZERO else Value.ZERO
         
 
     @staticmethod
     def four_of_a_kind(dice):
 
-        quarters = list(filter((lambda value: dice.count(value) >= FOUR), dice))        
-        return quarters[Pips.ONE.value] * FOUR if len(quarters) != ZERO else ZERO
+        quarters = list(filter((lambda value: dice.count(value) >= Value.FOUR), dice))        
+        return quarters[Pips.ONE.value] * Value.FOUR if len(quarters) != Value.ZERO else Value.ZERO
 
 
     @staticmethod
     def small_straight(dice):
 
-        ALL_PIPS = 5
         EXCLUDED_PIP = Pips.SIX.value
         
-        return Yatzy.chance(filter((lambda pip: Yatzy.chance(dice) if EXCLUDED_PIP not in dice and len(set(dice)) == ALL_PIPS else ZERO), dice))
+        return Yatzy.chance(filter((lambda pip: Yatzy.chance(dice) if EXCLUDED_PIP not in dice and len(set(dice)) == Value.FIVE else Value.ZERO), dice))
 
     @staticmethod
     def large_straight(dice):
         
-        ALL_PIPS = 5
         EXCLUDED_PIP = Pips.ONE.value
 
-        return Yatzy.chance(filter((lambda pip: Yatzy.chance(dice) if EXCLUDED_PIP not in dice and len(set(dice)) == ALL_PIPS else ZERO), dice))
+        return Yatzy.chance(filter((lambda pip: Yatzy.chance(dice) if EXCLUDED_PIP not in dice and len(set(dice)) == Value.FIVE else Value.ZERO), dice))
 
     @staticmethod
     def full_house(dice):
         
-        return Yatzy.chance(dice) if Yatzy.score_pair(dice) and Yatzy.three_of_a_kind(dice) else ZERO
+        return Yatzy.chance(dice) if Yatzy.score_pair(dice) and Yatzy.three_of_a_kind(dice) else Value.ZERO
 
